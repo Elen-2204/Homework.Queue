842532709bec74ddb1f0c6d2bb0198a93eaaf344
Elen Grigoryan <elen.v.grigoryan@gmail.com>
2024-10-22 15:41:04 +0400

    Add project files.

---
+++ Homework.Queue/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Homework.Queue;
+
+Queue<string> queue = new Queue<string>();
+
+queue.Enqueue("Hello");
+queue.Enqueue("World");
+queue.Enqueue("Bye");
+queue.Enqueue("something");
+
+
+queue.Dequeue();
+queue.Dequeue();
+
+
+Console.WriteLine(queue.Peek());
---
+++ Homework.Queue/Queue.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework.Queue
+{
+    public class Queue
+    {
+        private int[] _queueArray;
+        private int _front;
+        private int _rear;
+        private int _size;
+        private int _capacity;
+
+        public Queue(int capacity)
+        {
+            _capacity = capacity;
+            _queueArray = new int[_capacity];
+            _front = 0;
+            _rear = -1;
+            _size = 0;
+        }
+
+        public void Enqueue(int item)
+        {
+            if (_size == _capacity)
+            {
+                Console.WriteLine("Queue is full. ");
+                return;
+            }
+            _rear = (_rear + 1) % _capacity;
+            _queueArray[_rear] = item;
+            _size++;
+            Console.WriteLine($"Enqueued: {item}");
+        }
+
+        public int Dequeue()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty. ");
+            }
+            int item = _queueArray[_front];
+            _front = (_front + 1) % _capacity;
+            _size--;
+            return item;
+        }
+
+        public int Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty. ");
+            }
+            return _queueArray[_front];
+        }
+
+        public bool IsEmpty()
+        {
+            return _size == 0;
+        }
+
+        public int Size()
+        {
+            return _size;
+        }
+    }
+}
